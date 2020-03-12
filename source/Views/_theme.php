1fe15448bd87849122a09fa2399cef6589e843d8
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-03-11 23:03:21 -0300

    feat: add access to 'js' and 'css' folders

--- source/Views/_theme.php
+++ source/Views/_theme.php
@@ -1,13 +1,20 @@
+<?php
+/**
+ * @var $v
+ * @var string $title
+ */
+
+?>
 <!doctype html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?= $title; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title; ?></title>
 
-  <link rel="stylesheet" href="<?= url("source/style.css") ?>">
+    <link rel="stylesheet" href="<?= url("css/style.css") ?>">
     <?= $v->section("header"); ?>
 </head>
 <body>
@@ -15,6 +22,7 @@
     <?= $v->section("content"); ?>
 </main>
 
+<script src="<?= url("js/jquery.min.js"); ?>"></script>
 <?= $v->section("scripts"); ?>
 </body>
 </html>
--- source/Views/home/index.php
+++ source/Views/home/index.php
@@ -8,25 +8,44 @@
 $v->layout("_theme");
 ?>
 
-<header><?= $date ?></header>
+    <header><?= $date ?></header>
 
-<h1>To do List</h1>
+    <h1>To do List</h1>
 
-<div class="todos">
-    <?php
-    if ($todos): ?>
-        <ul>
-            <?php foreach ($todos as $item): ?>
-                <li>
-                    <input type="checkbox" id="<?= $item->cd_todo; ?>" <?= $item->st_todo == 1 ? "checked" : ""; ?>>
-                    <label for="<?= $item->cd_todo; ?>"><?= $item->nm_todo; ?></label>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <h2>Não existem tarefas cadastradas!</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, ratione!</p>
-    <?php endif; ?>
-</div>
+    <div class="todos">
+        <?php
+        if ($todos): ?>
+            <ul>
+                <?php foreach ($todos as $item): ?>
+                    <li>
+                        <input
+                            type="checkbox"
+                            id="<?= $item->cd_todo; ?>"
+                            <?= $item->st_todo == 1 ? "checked" : ""; ?>
+                            data-action="<?= $router->route("todo.update"); ?>"
+                            data-status="<?= $item->st_todo; ?>"
+                        >
+                        <label for="<?= $item->cd_todo; ?>"><?= $item->nm_todo; ?></label>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <h2>Não existem tarefas cadastradas!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, ratione!</p>
+        <?php endif; ?>
+    </div>
 
-<a class="button" href="<?= $router->route("todo.new"); ?>">Cadastrar</a>
+    <a class="button" href="<?= $router->route("todo.new"); ?>">Cadastrar</a>
+
+<?php $v->start("scripts"); ?>
+    <script>
+        $(function () {
+            $("body").on("click", "[data-action]", function (event) {
+                let data = $(this).data();
+                let li = $(this).parent();
+
+
+            });
+        })
+    </script>
+<?php $v->end();
